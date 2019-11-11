6c41039a9dc2640301895c32d7c93abe00ee1326
Dowie <dowied4@mymacewan.ca>
2019-11-11 05:57:42 -0700

    Added firebase auth, and initial enviroment vars for firebase.

--- src/recorder.js
+++ src/recorder.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { Grid, Button, Icon } from 'semantic-ui-react';
+import Axios from 'axios';
 
 class recorder extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
+		this.state = {
 			loaded: false,
 			recording: false
 		 }
 		 this.logout = this.logout.bind(this);
+		 this.startRecording = this.startRecording.bind(this);
+		 this.stopRecording = this.stopRecording.bind(this);
 	}
 
 	componentDidMount(){
+		if(!this.props.history.location.loggedIn){
+			this.props.history.push('/')
+		}
 		this.setState({
 			loaded: true
 		})
@@ -21,6 +27,38 @@ class recorder extends Component {
 	logout(){
 		this.props.history.push('/');
 	}
+
+	startRecording(){
+		this.setState({
+			recording: true
+		}, () => {
+			this.interval = setInterval(() => this.getMatch(), 1000);
+		})
+	}
+
+	stopRecording(){
+		console.log(this.interval)
+		this.setState({
+			recording: false
+		}, () => {
+			clearInterval(this.interval);
+		})
+	}
+
+	getMatch(){
+		Axios.get('/session')
+		.then((result => {
+			console.log(result.data.game_status);
+			//Send result to firestore
+		}))
+		.catch((err) => {
+			console.warn(err.response)
+		})
+	}
+
+	componentWillUnmount() {
+        clearInterval(this.interval)
+    }
 
 	render() { 
 		if(!this.state.loaded) {
@@ -34,13 +72,13 @@ class recorder extends Component {
 						onClick={() => {console.log("hello")}}/>
 					</Grid.Row>
 					<Grid.Row style={{marginTop: 100, marginBottom: 75}}>
-						{this.state.recording ? (<Button style={{width: 200}} animated negative size={"large"} onClick={() => this.setState({ recording: false })}>
+						{this.state.recording ? (<Button onMouseDown={e => e.preventDefault()} style={{width: 200}} animated negative size={"large"} onClick={this.stopRecording}>
 							<Button.Content visible>Stop Recording</Button.Content>
 							<Button.Content hidden>
 								<Icon name='window close' />
 							</Button.Content>
 						</Button>) :
-						(<Button style={{width: 200}} animated positive size={"large"} onClick={() => this.setState({ recording: true })}>
+						(<Button onMouseDown={e => e.preventDefault()} style={{width: 200}} animated positive size={"large"} onClick={this.startRecording}>
 							<Button.Content visible>Start Recording</Button.Content>
 							<Button.Content hidden>
 								<Icon name='video camera' />
--- src/signIn.js
+++ src/signIn.js
@@ -1,26 +1,94 @@
 import React, { Component } from 'react';
-import { Grid, Input, Button, Icon, Divider } from 'semantic-ui-react';
+import { Grid, Input, Button, Divider, Message, Transition } from 'semantic-ui-react'
+import * as firebase from "firebase/app";
+require('firebase/auth');
+require("firebase/firestore");
+
 
 class signIn extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { 
-			loaded: false
+		this.state = {
+			loaded: false,
+			error: {
+				status: false,
+				code: null,
+				message: null
+			}
 		 }
 		this.signIn = this.signIn.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.signIn = this.signIn.bind(this);
+		this.signUp = this.signUp.bind(this);
+
+		// .env variables for firebase
+		this.firebaseConfig = {
+			apiKey: process.env.REACT_APP_API_KEY,
+			authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+			databaseURL: process.env.REACT_APP_DATABASE,
+			projectId: process.env.REACT_APP_PROJECT_ID,
+			storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
+			messagingSenderId: process.env.REACT_APP_MESSAGINGID,
+			appId: process.env.REACT_APP_APP_ID,
+			measurementId: process.env.REACT_APP_MEASUREMENT
+		  };
 	}
 
 	componentDidMount(){
+		//Check if an instance of firebase app exists
+		if(!firebase.apps.length){
+			firebase.initializeApp(this.firebaseConfig)
+		}
 		this.setState({
 			loaded: true
 		})
 	}
 
-	signIn(){
-		console.log("sign in")
-		console.log(this.props.history)
-		this.props.history.push('/record')
+	handleChange = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		this.setState({
+			[name]: value
+		});
 	}
+
+	signIn(){
+		// comment out this auth stuff if you want to go to the recording screen to test
+		firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+		.then(() => {
+			//On successful login it pushes to next screen
+			this.props.history.push({
+				pathname: '/record',
+				loggedIn: true
+			})
+		})
+		.catch(err => {
+			console.log(err.code, err.message)
+			let tempErr = {
+				status: true,
+				code: err.code,
+				message: err.message
+			}
+			this.setState({
+				error: {...this.state.error, ...tempErr}
+			});
+		})
+		// uncomment below out to make Sign In redirect without firebase auth
+
+		// this.props.history.push({
+		// 	pathname: '/record',
+		// 	loggedIn: true
+		// })
+	}
+
+	signUp(){
+		//idk if we should have predetermined accounts for streamers but this signup is hear if we want it
+		firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+		.catch((err) => {
+			console.log(err.code, err.message)
+		})
+	}
+
 	render() {
 		if(!this.state.loaded){
 			return null
@@ -29,11 +97,25 @@ class signIn extends Component {
 				<Grid textAlign={'center'} verticalAlign={'middle'}>
 					<Grid.Row className="titlebar-window">
 						<h5 style={{top: 0, color: "white"}}>EchoVR Stream Buddy</h5>
-						<Button className="titlebar-close-button" circular size={"mini"} icon="close"
-						onClick={() => {console.log("hello")}}/>
+						<Button
+							className="titlebar-close-button"
+							circular
+							size={"mini"}
+							icon="close"
+							onClick={() => {console.log("y u no wurk")}}/>
 					</Grid.Row>
 					<Grid.Row style={{marginTop: 50}}>
 						<Grid.Column style={{ maxWidth: 450 }}>
+							<Transition visible={this.state.error.status} animation='scale' duration={500}>
+								<Message
+									name='authError'
+									error
+									negative
+									>
+									<Message.Header>An Error Occurred</Message.Header>
+									<p>{this.state.error.message}</p>
+								</Message>
+							</Transition>
 							<Input
 								fluid icon='user'
 								iconPosition='left'
@@ -59,7 +141,7 @@ class signIn extends Component {
 							<Button.Group fluid>
 								<Button positive onClick={this.signIn}>Sign In</Button>
 								<Button.Or />
-								<Button>Sign Up</Button>
+								<Button onClick={this.signUp}>Sign Up</Button>
 							</Button.Group>
 						</Grid.Column>
 					</Grid.Row>
@@ -68,5 +150,5 @@ class signIn extends Component {
 		}
 	}
 }
- 
+
 export default signIn;
