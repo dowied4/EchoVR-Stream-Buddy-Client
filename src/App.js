1d34460647de9534e54dbc83068eeac9e013e386
Dowie <dowied4@mymacewan.ca>
2019-11-19 15:20:03 -0700

    fixes typo in config

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ var config = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT
